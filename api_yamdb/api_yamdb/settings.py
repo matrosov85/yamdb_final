e8b866d45a92f2fe47b8ce89609b3cb39bc74431
Сергей Матросов <matrosov85@yandex.ru>
2023-05-03 15:27:40 +0300

    16th sprint 1st try

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -2,7 +2,6 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
